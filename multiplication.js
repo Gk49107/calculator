490d2c4ff862e5c69323be4b5ad287952824a2f0
unknown <guptaakanksha0017@gmail.com>
2023-01-02 11:00:34 +0530

    hii

--- multiplication.js
+++ multiplication.js
@@ -1,1 +1,9 @@
-multiplication.js
+function multiply()
+{
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result
+result = a*b
+ document.getElementById("result").value = result
+
+}
